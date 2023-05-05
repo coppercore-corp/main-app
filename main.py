501384f6dfe65195c3db75ac37bb0fea78114184
paulon-zervoulakus <paulon.zervoulakus@gmail.com>
2023-05-06 05:31:28 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/products", response_class=HTMLResponse)
+@app.get("/contacts", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("home/index.html", {"request": request})
