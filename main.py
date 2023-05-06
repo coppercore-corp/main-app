d477db344ca6a9be29033880a26e3f1ee254be79
paulon-zervoulakus <paulon.zervoulakus@gmail.com>
2023-05-06 16:29:54 +0800

    removing unused imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from pydantic import BaseModel
+#from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
