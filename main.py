138e69c18ffafda6a7cdbcfe9e65bfff478fddc0
paulon-zervoulakus <paulon.zervoulakus@gmail.com>
2023-05-06 17:51:49 +0800

    added test modules

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from fastapi import FastAPI, Request
-#from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/products", response_class=HTMLResponse)
-@app.get("/contacts", response_class=HTMLResponse)
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("home/index.html", {"request": request})
+    context = {
+        "request": request,
+        "title": "Welcome Home",
+        "url": "/",
+        "page": "Home"
+    }
+    return templates.TemplateResponse("home/index.html", context)
+
+
+@app.get("/products", response_class=HTMLResponse)
+def products(request: Request):
+    context = {
+        "request": request,
+        "title": "Our Products",
+        "url": "/products",
+        "page": "Products"
+    }
+    return templates.TemplateResponse("home/index.html", context)
+
+
+@app.get("/contacts", response_class=HTMLResponse)
+def contacts(request: Request):
+    context = {
+        "request": request,
+        "title": "Get in touch with us",
+        "url": "/contacts",
+        "page": "Contacts"
+    }
+    return templates.TemplateResponse("home/index.html", context)
---
+++ test/check_routes.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_routes():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "<title>CopperCore Corp. Home</title>" in response.text
+
+    response = client.get("/products")
+    assert response.status_code == 200
+    assert "<title>CopperCore Corp. Products</title>" in response.text
+
+    response = client.get("/contacts")
+    assert response.status_code == 200
+   assert "<title>CopperCore Corp. Contacts</title>" in response.text
