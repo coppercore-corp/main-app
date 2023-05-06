299535a220eac8be8ca2753fdf83ba521252441e
paulon-zervoulakus <paulon.zervoulakus@gmail.com>
2023-05-06 18:38:42 +0800

    fixing imports

--- test/check_routes.py
+++ test/check_routes.py
@@ -1,10 +1,14 @@
 import sys
 import os
-from fastapi.testclient import TestClient
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from main import app
+from fastapi.testclient import TestClient
+from importlib import import_module
+main = import_module('main')
+app = getattr(main,"app")
+
+# from main import app
 
 client = TestClient(app)
 
