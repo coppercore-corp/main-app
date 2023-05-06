8c7e2cf2f5a297a634dc35d3dc40a894dde11b72
paulon-zervoulakus <paulon.zervoulakus@gmail.com>
2023-05-06 18:26:22 +0800

    fix test route_check.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+templates = Jinja2Templates(directory=template_dir)
 
 
 @app.get("/", response_class=HTMLResponse)
