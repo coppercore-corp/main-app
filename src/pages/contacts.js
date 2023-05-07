bb6ac24a0e150542a83e6becc20614effc1d86e4
paulon-zervoulakus <paulon.zervoulakus@gmail.com>
2023-05-07 18:10:43 +0800

    completed ver.1

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -7,11 +7,11 @@ function Contacts() {
                 <h1>Contact Us</h1>    
                 <span>If you have a question about us, need more information, or want to make a suggestion, please use any of the following options to contact us:</span>
                 <ul>
-                    <li>Taguig Office 1: Block 1 lot 49 Enlisted Personnel Phase 2 Taguig City, Philippines</li>
-                    <li>Taguig C5 Office2: Lot 15 Block 45 Western Bicutan Taguig City, Philippines</li>
-                    <li>Taguig Landline: (02)6689345; (02)5198295</li>
-                    <li>Cell: (0947)4420509</li>
-                    <li>Email: pzervoulakus.trading@yahoo.com.ph</li>
+                    <li>Level 10-1 Fort Legend Tower 3rd Ave. Corner 31st street</li>
+                    <li>Bonifacio Global Taguig City, Philippines</li>
+                    <li>Taguig Landline: (632)529-0698</li>
+                    <li>Cell: (0910)361-1750 or (0926)092-8228</li>
+                    <li>Email: sales-admin@ccorecorp.com</li>
                 </ul>            
             </div>
         </div>
